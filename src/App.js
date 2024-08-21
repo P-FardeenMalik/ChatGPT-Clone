2aad22930e47bd5d42d62907cc14c8363b830bbb
P-FardeenMalik <fardeenmalik138@gmail.com>
2024-08-21 23:13:17 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop"> <img src="" alt="" className="logo" /> <span className="brand">ChatGPT</span></div>
+          <button className="midBtn"> <img src="" alt="" className="addBtn" />New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"> <img src="" alt="" />What is Programming ?</button>
+            <button className="query"> <img src="" alt="" />What is Programming ?</button>
+          </div>
 
+        </div>
+
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
